b4bc134f0ab146d4455be4e270784f27bf438c20
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2021-10-07 23:26:17 +0700

    Mengirim folder file project latihan 15

---
+++ latihan15/utils/contacts.js
@@ -0,0 +1,36 @@
+// Memanggil Core Module
+// File System
+const fs = require('fs');
+
+// Membuat folder data jika belum ada
+// Apakah ada folder data ?, jika belum ada maka akan dibuatkan
+const dirPath = './data';
+if(!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+// Membuat file contacts.json jika belum ada
+// Apakah ada file data path ?, jika belum ada maka akan dibuatkan
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+};
+
+// Mengambil semua data di contact.json
+const loadContact = () => {
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+    return contacts;
+};
+
+// Mencari contact berdasarkan nama
+const findContact = (nama) => {
+    const contacts = loadContact();
+
+    // Mencari kontak kalau misalkan datanya ketemu, kalau tidak maka undefined
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+    return contact;
+};
+
+// Mengeksport file
+module.exports = {loadContact, findContact}
